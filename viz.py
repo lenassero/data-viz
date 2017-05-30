78a1e342a2207075faca78183f2598314fedd210
Nasser Benabderrazik <benab.nasser@gmail.com>
2017-05-30 13:16:25 +0000

    Add plot value counts for categorical variables of a DataFrame

---
+++ viz.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+
+
+def subplot_value_counts(data, variable, width = 0.8):
+    """ Plot value counts for one categorical variable.
+
+    Parameters
+    ----------
+
+	data: DataFrame
+		DataFrame with categorical variables
+    variable: string
+    	Name of the categorical variable
+    width: int, optional
+    	Width of the bars
+
+    Returns
+    -------
+
+    barplot: single bar plot
+    """
+
+
+    # Value counts (limit to 30 categories)
+    counts = data[variable].value_counts()[:30]
+
+    # Number of categories 
+    n_categories = len(counts)
+
+    # Categories
+    categories = list(counts.index)
+
+    # Plot the bars 
+    barplot = plt.bar(height = counts, left = range(n_categories), 
+    	              color = "green", width = 0.8)
+
+    # Change the color of the Missing Values bar 
+    if "Missing values" in categories:
+        barplot[categories.index("Missing values")].set_color("r")
+
+    # Add the categories as the x labels 
+    ticks = [sum(x) for x in zip(range(n_categories), [width/2] * n_categories)]
+    plt.xticks(ticks, categories, rotation = 80)
+
+    # Add a title and adjust the distance to the figure
+    plt.title("'{}' category counts".format(variable), fontsize = 14, y = 1.02)
+
+    return barplot
+
+
+def plot_value_counts(data, variables):
+    """ Plot value counts of the different categorical variables.
+
+    Parameters
+    ----------
+
+	data: DataFrame
+		DataFrame with categorical variables
+    variable: list
+    	Names of the categorical variables
+    width: int, optional
+    	Width of the bars
+    """ 
+
+    # Replace NaN values to be able to show them as a category
+    data = data.fillna("Missing values")
+
+    # Number of categorical variables specified
+    n_variables = len(variables)
+
+    plt.figure(figsize = (14,20))
+
+    # Two subplots per row
+    numrows = round(n_variables / 2)
+    
+    for i in range(n_variables):
+    	# Two subplots per row
+        plt.subplot(numrows, 2, i+1)
+        subplot_value_counts(data, variables[i])
+
+    # Adjust the spacing between subplots
+    plt.subplots_adjust(wspace = None, hspace = 1)
+
+    # Show the figure containing the subplots
+    plt.show()
